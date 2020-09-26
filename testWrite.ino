b8e506ba0621a9aa2b7a80b4212b880e4b875a83
Mathias LIen <mathilie@stud.ntnu.no>
2020-09-26 18:53:50 +0200

    startet på den generelle arduinologikken

--- testWrite.ino
+++ testWrite.ino
@@ -2,6 +2,10 @@
 // Serial test script
 int buttonPins[] = {4, 7};
 int nrOfButtons = 2;
+int digitalPins[];
+int digitalVals[];
+int analogPins[];
+int analogVals[];
 
 void setup()
 {
@@ -15,7 +19,33 @@ void setup()
 
 void loop()
 {
-  //while(!Serial.available()) {}
+  for(int i=0; i<(sizeof(digitalPins)/sizeof(digitalPins[0]));i++){
+    checkDigitalInputs(i);
+  }
+  for(int j=0; j<(sizeof(analogPins)/sizeof(analogPins[0]));j++){
+    checkAnalogInputs(j);
+  }
+}
+
+
+
+void checkAnalogInputs(int i){
+}
+
+
+void checkDigitalInputs(int i){
+  int state = digitalRead(digitalPins[i]);
+  if(state!=digitalVals[i]){
+    Serial.print();
+    Serial.print("\n");
+    Serial.flush();
+    delay(10);
+  }
+}
+
+
+void GAMMELKODE(){
+      //while(!Serial.available()) {}
   delay(1000);
 
   // serial write section
